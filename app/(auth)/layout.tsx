3a876e25b5ae5f45548a88d3cf357a64e7f94fd9
EnricoRP <email@enricorp.com>
2025-04-17 11:22:45 +0700

    perbaikan tampilan sign in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+
  export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="flex min-h-screen w-full justify-between font-inter">
         {children}
+        <div className="auth-asset">
+          <Image src='/icons/auth-image.svg' height={500} width={500} alt='auth-image'/>
+        </div>
     </main>
   );
 }
