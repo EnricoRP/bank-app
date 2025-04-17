e5c54486b55a49ce2e4e0f1f6072dc218a6c5316
EnricoRP <email@enricorp.com>
2025-04-17 11:21:59 +0700

    pengecekan user sudah login atau belum, jika tidak di cek maka setelah logout masih berada di dashboard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,15 @@ import MobileNav from "@/components/ui/MobileNav";
 import Sidebar from "@/components/ui/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.action";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) { 
-  const loggedIn = await getLoggedInUser()
+  const loggedIn = await getLoggedInUser();
+  if(!loggedIn) redirect('/sign-in');
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn}/>
