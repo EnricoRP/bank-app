0060daa058f8b01cc24436fa177d209bb31c9aaf
EnricoRP <email@enricorp.com>
2025-04-23 16:17:39 +0700

    bug fix tombol view all karena pagination belum di implementasikan

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -11,7 +11,7 @@ const RecentTransactions = ({ accounts, transactions, appwriteItemId, page }: Re
 
             <header className="flex items-center justify-between">
                 <h2 className='recent-transactions-label'>Recent Transactions</h2>
-                <Link href={`/transactions-history/?id=${appwriteItemId}?page=${page}`} className="view-all-btn">
+                <Link href={`/transaction-history/?id=${appwriteItemId}`} className="view-all-btn">
                     View All
                 </Link>
             </header>
