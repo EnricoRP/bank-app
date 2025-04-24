eb2e094a5c5ac061e12d547a79a53f613422f827
EnricoRP <email@enricorp.com>
2025-04-24 10:51:52 +0700

    developing halaman payment transfer

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,10 +1,27 @@
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import HeaderBox from '@/components/ui/HeaderBox'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Transfer = () => {
+const Transfer = async() => {
+    const looggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({
+      userId: looggedIn?.$id
+    });
+    if(!accounts) return;
+    const accountsData = accounts?.data;
   return (
-    <div>
-      
-    </div>
+    <section className="payment-transfer">
+      <HeaderBox
+        title='Payment Transfer'
+        subtext='Please provide any specific detail or notes related to the payment transfer.'
+      />
+
+      <section className="size-full pt-5">
+          <PaymentTransferForm accounts={accountsData} />
+      </section>
+    </section>
   )
 }
 
