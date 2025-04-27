3b27d2ecb8c30177e85ff1b2667f9b68d7f9867f
riski <enricoriski@gmail.com>
2025-04-27 22:25:33 +0700

    implementing function withAuthPage & withAuthPageForAccount

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,28 @@
 import MobileNav from "@/components/ui/MobileNav";
-import Sidebar from "@/components/ui/Sidebar";
-import { getLoggedInUser } from "@/lib/actions/user.actions";
-import Image from "next/image";
-import { redirect } from "next/navigation";
+import Sidebar from "@/components/ui/Sidebar"; 
+import { withAuthPage } from "@/lib/utils";
+import Image from "next/image"; 
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) { 
-  const loggedIn = await getLoggedInUser();
-  if(!loggedIn) redirect('/sign-in');
-  return (
-    <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn}/>
-      <div className="flex size-full flex-col">
-        <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-          <div>
-            <MobileNav user={loggedIn} />
+}>) {
+
+  return withAuthPage(async (loggedIn) => {
+    return (
+      <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
           </div>
+          {children}
         </div>
-        {children}
-      </div>
-    </main>
-  );
+      </main>
+    );
+  });
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,48 +3,50 @@ import HeaderBox from '@/components/ui/HeaderBox'
 import RightSidebar from '@/components/ui/RightSidebar'
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
-import { getLoggedInUser } from '@/lib/actions/user.actions'
+import { withAuthPage } from '@/lib/utils'
 import React from 'react'
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
-  const currentPage = Number(page as string) || 1;
-  const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({
-    userId: loggedIn.$id
+  return withAuthPage(async (loggedIn) => {
+    const currentPage = Number(page as string) || 1;
+    const accounts = await getAccounts({
+      userId: loggedIn.$id
+    });
+
+    if (!accounts) return;
+    const accountsData = accounts?.data;
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+    const account = await getAccount({ appwriteItemId });
+
+    return (
+      <section className='home'>
+        <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox
+              type='greeting'
+              title='Welcome,'
+              subtext='Access & manage your account and transactions efficiently.'
+              user={loggedIn?.firstName || 'Guest'}
+            />
+
+            <TotalBalanceBox
+              accounts={accountsData}
+              totalBanks={accounts?.totalBanks}
+              totalCurrentBalance={accounts?.totalCurrentBalance}
+            />
+          </header>
+
+          <RecentTransactions
+            accounts={accountsData}
+            transactions={account?.transactions}
+            appwriteItemId={appwriteItemId}
+            page={currentPage} />
+        </div>
+        <RightSidebar user={loggedIn} transactions={account?.transactions} banks={accountsData?.slice(0, 2)} />
+      </section>
+    )
   });
 
-  if (!accounts) return;
-  const accountsData = accounts?.data;
-  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-  const account = await getAccount({ appwriteItemId });
-  
-  return (
-    <section className='home'>
-      <div className='home-content'>
-        <header className='home-header'>
-          <HeaderBox
-            type='greeting'
-            title='Welcome,'
-            subtext='Access & manage your account and transactions efficiently.'
-            user={loggedIn?.firstName || 'Guest'}
-          />
-
-          <TotalBalanceBox
-            accounts={accountsData}
-            totalBanks={accounts?.totalBanks}
-            totalCurrentBalance={accounts?.totalCurrentBalance}
-          />
-        </header>
-
-        <RecentTransactions
-          accounts={accountsData}
-          transactions={account?.transactions}
-          appwriteItemId={appwriteItemId}
-          page={currentPage} />
-      </div>
-      <RightSidebar user={loggedIn} transactions={account?.transactions} banks={accountsData?.slice(0, 2)} />
-    </section>
-  )
 }
 
 export default Home
